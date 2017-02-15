425aa49c47ef2aba2f796a0d34bec21f402858c2
Alex Seaver <cinaminatastic@gmail.com>
2017-02-15 13:21:31 -0500

    First Commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/********************************************************************
+Main for Project 3 Pong Program by Alex Seaver and Coire Gavin Hanner
+Last Modified: February 15
+********************************************************************/
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main()
+{
+    cout << "Hello World\n";
+
+    system("PAUSE");
+    return 0;
+}
