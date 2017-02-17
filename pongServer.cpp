1f9a7c2c58218f09a6a77525562b3c7f0fc165a4
coiregavhan <gavinhannerc@kenyon.edu>
2017-02-17 15:36:08 -0500

    added basic fifo for sending and receiving a single name

--- pongServer.cpp
+++ pongServer.cpp
@@ -1,29 +1,55 @@
 /*************************************************************/
 /* Authors: Alex Seaver and Coire Gavin-Hanner               */
+/* Date Created: 2-17-17					                 */
 /*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*                                                           */
-/*************************************************************/
+/* Variables:
+		ballx, bally
+		pad1y, pad1x, pad2y, pad2x
+		padw, padh (same for both paddles)
+		height
+		*/
 
 
 
 #include <iostream>
-#include <stdlib>,
+#include <stdlib>
+#include "fifo.h"
 using namespace std;
 
 int main()
 {
-  cout << "Server to eventually run pong\n";
+	string p1Name, p2Name;
+	string nameRec_fifo = "receivename";
+	string nameSend_fifo = "sendname";
+	
+	cout << "test: main_1"
+	// create the FIFOs for communication
+	Fifo nameRecfifo(nameRec_fifo);
+	Fifo nameSendfifo(nameSend_fifo);
 
-  system("pause");
-  return 0;
+	cout << "test: main_2 (fifo's created)"
+
+	nameRecfifo.openread();
+	p1Name = nameRecfifo.recv();
+	nameRecfifo.fifoclose();	
+	cout << "test: main_3 (name received)"
+	cout << "Name: " << p1Name << endl;
+
+	nameSendfifo.openwrite();
+	nameSendfifo.send(p1Name);
+	nameSendfifo.fifoclose;	
+	cout << "test: main_4 (name sent)"
+
+	
+	
+	cout << "Server to eventually run pong\n";
+	
+	while(1)
+	{
+		
+	}
+	
+
+	//system("pause");
+	return 0;
 }
