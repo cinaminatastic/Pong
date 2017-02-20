35318bb6365d991c32dc338bfd2264cac39e9030
coiregavhan <gavinhannerc@kenyon.edu>
2017-02-19 20:09:11 -0500

    update pongAjax

--- pongAjax.cpp
+++ pongAjax.cpp
@@ -3,9 +3,44 @@
 /*************************************************************/
 
 #include <iostream>
+// Stuff for AJAX
+#include "cgicc/Cgicc.h"
+#include "cgicc/HTTPHTMLHeader.h"
+#include "cgicc/HTMLClasses.h"
+
+//Stuff for pipes
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <iostream>
+#include <fstream>
+#include "fifo.h"
+
 using namespace std;
+using namespace cgicc; // Needed for AJAX functions.
 
 int main()
 {
-  return 0;
+	string nameRec_fifo = "receivename";
+	string nameSend_fifo = "sendname";
+
+	Cgicc cgi;    // Ajax object
+	
+	string name = cgi.getElement("name");
+	
+	nameSend_fifo.openwrite();
+	nameSend_fifo.send(name);
+	nameSend_fifo.fifoclose();
+	
+	nameRec_fifo.openread();
+	string recName = nameRec_fifo.recv();
+	nameRec_fifo.fifoclose();
+	
+	cout << recName;
+	
+	
+	return 0;
 }
